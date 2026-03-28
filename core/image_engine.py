e1838db3caf0109eb94b8c9626fc6a3bac681b24
VijaiVenkatesan <92427958+VijaiVenkatesan@users.noreply.github.com>
2026-03-28 20:50:35 +0530

    Update image_engine.py

--- core/image_engine.py
+++ core/image_engine.py
@@ -1,1 +1,11 @@
+import requests
+from PIL import Image
+from io import BytesIO
 
+def generate_image(prompt):
+    try:
+        url = f"https://image.pollinations.ai/prompt/{prompt}"
+        res = requests.get(url)
+        return Image.open(BytesIO(res.content))
+    except:
+        return None
