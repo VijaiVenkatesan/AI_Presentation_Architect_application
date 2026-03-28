1eb60dd2d89794b389026e71cbc14fb5c4db2a9a
VijaiVenkatesan <92427958+VijaiVenkatesan@users.noreply.github.com>
2026-03-28 22:53:24 +0530

    Update search_engine.py

--- core/search_engine.py
+++ core/search_engine.py
@@ -1,9 +1,10 @@
-from duckduckgo_search import DDGS
+from ddgs import DDGS
 
 def fetch_search_data(query):
     try:
         with DDGS() as ddgs:
             results = ddgs.text(query, max_results=5)
             return " ".join([r["body"] for r in results])
-    except:
+    except Exception as e:
+        print("Search error:", e)
         return ""
